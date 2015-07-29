aafa83f3d523dfe24a8a1d323d6ffce4b654fb52
RZR <franche.vandenberg@gmail.com>
2015-07-29 20:34:58 +0200

    Initial Commit

---
+++ SimpleTimer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SimpleTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrSimple = new System.Windows.Forms.Timer(this.components);
+            this.btnStartTimer = new System.Windows.Forms.Button();
+            this.btnEndTimer = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrSimple
+            // 
+            this.tmrSimple.Interval = 1000;
+            this.tmrSimple.Tick += new System.EventHandler(this.tmrSimple_Tick);
+            // 
+            // btnStartTimer
+            // 
+            this.btnStartTimer.Location = new System.Drawing.Point(12, 12);
+            this.btnStartTimer.Name = "btnStartTimer";
+            this.btnStartTimer.Size = new System.Drawing.Size(95, 23);
+            this.btnStartTimer.TabIndex = 0;
+            this.btnStartTimer.Text = "Start Timer";
+            this.btnStartTimer.UseVisualStyleBackColor = true;
+            this.btnStartTimer.Click += new System.EventHandler(this.btnStartTimer_Click);
+            // 
+            // btnEndTimer
+            // 
+            this.btnEndTimer.Location = new System.Drawing.Point(238, 12);
+            this.btnEndTimer.Name = "btnEndTimer";
+            this.btnEndTimer.Size = new System.Drawing.Size(91, 23);
+            this.btnEndTimer.TabIndex = 1;
+            this.btnEndTimer.Text = "End Timer";
+            this.btnEndTimer.UseVisualStyleBackColor = true;
+            this.btnEndTimer.Click += new System.EventHandler(this.btnEndTimer_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(142, 15);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(64, 17);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 47);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnEndTimer);
+            this.Controls.Add(this.btnStartTimer);
+            this.Name = "Form1";
+            this.Text = "Simple Timer Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrSimple;
+        private System.Windows.Forms.Button btnStartTimer;
+        private System.Windows.Forms.Button btnEndTimer;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ SimpleTimer/Form1.cs
@@ -0,0 +1,63 @@
+﻿#region Using Section
+
+using System;
+using System.Windows.Forms;
+
+#endregion
+
+namespace SimpleTimer
+{
+    public partial class Form1 : Form
+    {
+        private static DateTime sessionDateTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnEndTimer.Enabled = false;
+        }
+
+        private void tmrSimple_Tick(object sender, EventArgs e)
+        {
+            // Using String format and building a string using the Current DateTime.Now object to minus the started DateTime Object from it.
+            // To get the difference between the 2 DateTime values. This then gives me the hours/minutes/seconds
+            // I then grab the values from it.
+
+            var dateTimeDiff = (DateTime.Now - sessionDateTime);
+
+            lblTime.Text = string.Format("{0}:{1}:{2}", DecimalPlacesForced2(dateTimeDiff.Hours),
+                DecimalPlacesForced2(dateTimeDiff.Minutes),
+                DecimalPlacesForced2(dateTimeDiff.Seconds));
+        }
+
+        private void btnStartTimer_Click(object sender, EventArgs e)
+        {
+            btnEndTimer.Enabled = true;
+            btnStartTimer.Enabled = false;
+
+            sessionDateTime = DateTime.Now;
+            lblTime.Text = @"00:00:00";
+
+            // Starts the timer object that then executes it own thread to handle the tmrSimple_Tick(object sender, EventArgs e) Event.
+            tmrSimple.Start();
+        }
+
+        private void btnEndTimer_Click(object sender, EventArgs e)
+        {
+            btnEndTimer.Enabled = false;
+            btnStartTimer.Enabled = true;
+
+            // Remember to record the end DateTime here... :)
+
+            // Stops the timer...
+            tmrSimple.Stop();
+        }
+
+        // This method only adds a 0 if the time value is less than 10. Example: if seconds is 5 then a 0 is added in front of it.
+        // so then the value is 05. Else if the value is larger than 10 then only the value is returned.
+        private static string DecimalPlacesForced2(int time)
+        {
+            return time < 10 ? string.Format("0{0}", time) : time.ToString();
+        }
+    }
+}
